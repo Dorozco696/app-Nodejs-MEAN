5102e6de7459619bdbbd8a617ed7791472ba51ae
unknown <diegougsbba@gmail.com>
2026-07-12 18:09:13 -0600

    correcion de duplicados

--- depligue-MEAN-terraform-main/modules/web/main.tf
+++ depligue-MEAN-terraform-main/modules/web/main.tf
@@ -18,15 +18,3 @@ EOF
     Name = "${var.project_name}-web-${count.index + 1}"
   }
 }
-
-output "web_instance_ids" {
-  value = aws_instance.web[*].id
-}
-
-output "web_private_ips" {
-  value = aws_instance.web[*].private_ip
-}
-
-output "web_public_ips" {
-  value = aws_instance.web[*].public_ip
-}
--- depligue-MEAN-terraform-main/modules/web/outputs.tf
+++ depligue-MEAN-terraform-main/modules/web/outputs.tf
@@ -2,6 +2,10 @@ output "web_instance_ids" {
   value = aws_instance.web[*].id
 }
 
+output "web_private_ips" {
+  value = aws_instance.web[*].private_ip
+}
+
 output "web_public_ips" {
   value = aws_instance.web[*].public_ip
 }
